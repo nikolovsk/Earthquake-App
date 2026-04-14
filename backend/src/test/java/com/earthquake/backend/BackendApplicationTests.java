3eac39057e58e934393dcb098ff014e6cec8b45f
nikolovsk <nikolovskangelaa@gmail.com>
2026-04-14 21:49:02 +0200

    Initial project setup

---
+++ backend/src/test/java/com/earthquake/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.earthquake.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
